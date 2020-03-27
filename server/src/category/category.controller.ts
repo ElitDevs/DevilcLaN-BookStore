005c7a086be2302ad1b7394100449ed607eee0c7
Baraka Larbi <laridev@gmail.com>
2020-03-27 23:03:53 +0100

    formating

--- server/src/category/category.controller.ts
+++ server/src/category/category.controller.ts
@@ -1,42 +1,47 @@
-import { Controller,Param,Body, Get , Post , Put, Delete , Logger  } from '@nestjs/common';
+import {
+  Controller,
+  Param,
+  Body,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Logger,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryDto } from './category.dto';
 
 @Controller('api/categories')
 export class CategoryController {
-    private logger = new Logger('====== Category Controller ====');
-    constructor(private categoryService : CategoryService){}
+  private logger = new Logger('====== Category Controller ====');
+  constructor(private categoryService: CategoryService) {}
 
-    @Get()
-    getAll(){
-        return this.categoryService.findAll();
-    }
+  @Get()
+  getAll() {
+    return this.categoryService.findAll();
+  }
 
-    @Get(':id')
-    getOne(@Param('id') id: number){
-        this.logger.log(JSON.stringify(id));
-        return this.categoryService.findOne(id);
-    }
+  @Get(':id')
+  getOne(@Param('id') id: number) {
+    this.logger.log(JSON.stringify(id));
+    return this.categoryService.findOne(id);
+  }
 
+  @Post()
+  post(@Body() data: CategoryDto) {
+    this.logger.log(JSON.stringify(data));
+    return this.categoryService.create(data);
+  }
 
-    @Post()
-    post(@Body() data : CategoryDto){
-        this.logger.log(JSON.stringify(data));
-        return this.categoryService.create(data);
-    }
+  @Put(':id')
+  update(@Param('id') id: number, @Body() data: CategoryDto) {
+    this.logger.log(JSON.stringify(data));
+    return this.categoryService.update(id, data);
+  }
 
-
-    @Put(':id')
-    update(@Param("id") id: number, @Body() data : CategoryDto){
-        this.logger.log(JSON.stringify(data));
-        return this.categoryService.update(id, data);
-    }
-
-    @Delete(':id')
-    destroy(@Param('id') id : number){
-        this.logger.log(JSON.stringify(id));
-        return  this.categoryService.remove(id);
-    }
-
-
+  @Delete(':id')
+  destroy(@Param('id') id: number) {
+    this.logger.log(JSON.stringify(id));
+    return this.categoryService.remove(id);
+  }
 }
