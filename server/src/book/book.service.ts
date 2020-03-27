3d399a9fd15518c2b1f6d643942b122a7a4ad3dd
Baraka Larbi <laridev@gmail.com>
2020-03-27 18:27:43 +0100

    creating functions in book service

--- server/src/book/book.service.ts
+++ server/src/book/book.service.ts
@@ -1,4 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { BookEntity } from './book.entity';
+import { BookDto } from './book.dto';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+
+    constructor(
+        @InjectRepository(BookEntity)
+        private bookRepository : Repository<BookEntity>
+    ){}
+
+    async findAll(): Promise<BookEntity[]> {
+        return await this.bookRepository.find();
+    }
+
+    async read(id: string):Promise<BookEntity>{
+        const book = await this.bookRepository.findOne({id});
+        if(!book){
+            throw new HttpException('Not Found', HttpStatus.BAD_REQUEST);
+        }
+        return await this.bookRepository.findOne({id})
+    }
+
+    async create(data : BookDto):Promise<BookEntity>{
+        const book = await this.bookRepository.create({...data});
+        await this.bookRepository.save(book);
+        return book;
+    }
+
+    async update(id: string, data: BookDto):Promise<BookEntity>{
+        const book = await this.bookRepository.findOne({id});
+        if(!book){
+            throw new HttpException('Not Found', HttpStatus.BAD_REQUEST);
+        }
+        await this.bookRepository.update({id}, data);
+        return book; 
+    }
+
+    async remove(id:string){
+        const book = await this.bookRepository.findOne({id});
+        if(!book){
+            throw new HttpException('Not Found', HttpStatus.BAD_REQUEST);
+        }
+        this.bookRepository.delete(book);
+        return book;
+    }
+
+}
