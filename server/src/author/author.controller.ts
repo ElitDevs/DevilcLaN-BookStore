f7b8f8b9fc702abafbe72965d6122baa82306e92
Baraka Larbi <laridev@gmail.com>
2020-03-27 23:36:06 +0100

    adding author controller

--- server/src/author/author.controller.ts
+++ server/src/author/author.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  Logger,
+} from '@nestjs/common';
+import { AuthorService } from './author.service';
+import { AuthorDto } from './author.dto';
 
-@Controller('author')
-export class AuthorController {}
+@Controller('api/authors')
+export class AuthorController {
+  private logger = new Logger('==== Author Controller ====');
+
+  constructor(private authorService: AuthorService) {}
+
+  @Get()
+  getAll() {
+    return this.authorService.findAll();
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string) {
+    this.logger.log(JSON.stringify(id));
+    return this.authorService.read(id);
+  }
+
+  @Post()
+  post(@Body() data: AuthorDto) {
+    this.logger.log(JSON.stringify(data));
+    return this.authorService.create(data);
+  }
+
+  @Put(':id')
+  put(@Param('id') id: string, @Body() data: AuthorDto) {
+    this.logger.log(JSON.stringify(data));
+    return this.authorService.update(id, data);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    this.logger.log(JSON.stringify(id));
+    return this.authorService.delete(id);
+  }
+}
