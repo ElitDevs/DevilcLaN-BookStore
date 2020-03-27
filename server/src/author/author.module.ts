ed214f263b23c2cf92382894fa3e6ed4e58bee11
Baraka Larbi <laridev@gmail.com>
2020-03-27 23:26:43 +0100

    adding author service

--- server/src/author/author.module.ts
+++ server/src/author/author.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthorController } from './author.controller';
 import { AuthorService } from './author.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthorEntity } from './author.entity';
 
 @Module({
-  controllers: [AuthorController],
+  imports : [TypeOrmModule.forFeature([AuthorEntity])],
+controllers: [AuthorController],
   providers: [AuthorService]
 })
 export class AuthorModule {}
--- server/src/author/author.service.ts
+++ server/src/author/author.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthorEntity } from './author.entity';
+import { Repository } from 'typeorm';
+import { AuthorDto } from './author.dto';
 
 @Injectable()
-export class AuthorService {}
+export class AuthorService {
+  private logger = new Logger('AuthorService');
+
+  constructor(
+    @InjectRepository(AuthorEntity)
+    private authorRepository: Repository<AuthorEntity>,
+  ) {}
+
+  async findAll(): Promise<AuthorEntity[]> {
+    return await this.authorRepository.find();
+  }
+
+  async read(id: string): Promise<AuthorEntity> {
+    return await this.authorRepository.findOne({ id });
+  }
+
+  async create(data: AuthorDto): Promise<AuthorEntity> {
+    const author = await this.authorRepository.create({ ...data });
+    return await this.authorRepository.save(author);
+  }
+
+  async update(id: string, data: Partial<AuthorDto>): Promise<AuthorEntity> {
+    const author = await this.authorRepository.findOne({ id });
+    if (!author) {
+      throw new HttpException('Not Found', HttpStatus.BAD_REQUEST);
+    }
+    await this.authorRepository.update({ id }, data);
+    return author;
+  }
+
+  async delete(id: string) {
+    const author = await this.authorRepository.findOne({ id });
+    if (!author) {
+      throw new HttpException('Not Found', HttpStatus.BAD_REQUEST);
+    } 
+    await this.authorRepository.remove(author);
+    return true;
+  }
+}
