3662caf1bc09b0320c380544b995ae1b754d6ea5
Baraka Larbi <laridev@gmail.com>
2020-03-27 15:39:43 +0100

    adding bookdto

--- server/src/book/book.dto.ts
+++ server/src/book/book.dto.ts
@@ -1,3 +1,19 @@
+import { IsString, IsNumber } from 'class-validator';
+/**
+ * @class BookDto
+ * 
+ */ 
 export class BookDto {
-    
+  @IsString()
+  bookname: string;
+  @IsNumber()
+  bookpages: number;
+  @IsString()
+  bookisbn: string;
+  @IsNumber()
+  bookprice: number;
+  @IsString()
+  bookpicture: string;
+  @IsNumber()
+  bookquntity: number;
 }
