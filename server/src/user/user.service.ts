23814488c5093984d0e73d7409e3e6df623447a9
Baraka Larbi <laridev@gmail.com>
2020-03-28 16:04:06 +0100

    creating user service

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './user.entity';
+import { Repository } from 'typeorm';
+import { UserDto } from './user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private logger = new Logger('User Service');
+
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {}
+
+  async findAll(): Promise<UserEntity[]> {
+    return await this.userRepository.find();
+  }
+
+  async read(id: number): Promise<UserEntity> {
+    return await this.userRepository.findOne({ id });
+  }
+
+  async create(data: Partial<UserDto>): Promise<UserEntity> {
+    const user = await this.userRepository.create({ ...data });
+    return await this.userRepository.save(user);
+  }
+
+  async update(id: number, data: Partial<UserDto>): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({ id });
+    if (!user) {
+      throw new HttpException('Not Found', HttpStatus.BAD_REQUEST);
+    }
+    await this.userRepository.update({ id }, data);
+    return user;
+  }
+
+  async delete(id: number) {
+    const user = await this.userRepository.findOne({ id });
+    if (!user) {
+      throw new HttpException('Not Found', HttpStatus.BAD_REQUEST);
+    }
+    await this.userRepository.remove(user);
+    return true;
+  }
+}
