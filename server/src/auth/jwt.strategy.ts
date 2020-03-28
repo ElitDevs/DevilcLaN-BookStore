89a7d3bc77e6c6e68af1d13a8f95f459b711b8ea
Baraka Larbi <laridev@gmail.com>
2020-03-28 20:31:20 +0100

    initiate jwt

--- server/src/auth/jwt.strategy.ts
+++ server/src/auth/jwt.strategy.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { Strategy, ExtractJwt, VerifiedCallback } from 'passport-jwt';
+import { AuthService } from './auth.service';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {}
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    // because we are using actual inheretance we need
+    // to use super method
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'secretKey',
+    });
+  }
+
+  //   async validate(payload: any, done: VerifiedCallback) {
+  //     return done(null, user, payload.iat);
+  //   }
+}
