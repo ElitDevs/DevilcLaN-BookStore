e4f0f591bcdaf83fe61a637141aab7cc63d80d51
Baraka Larbi <laridev@gmail.com>
2020-03-28 14:54:55 +0100

    creating user entity

---
+++ server/src/user/user.entity.ts
@@ -0,0 +1,66 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('User')
+export class UserEntity {
+  @PrimaryGeneratedColumn() id: number;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    unique: true,
+  })
+  username: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+  })
+  password: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+  })
+  firstname: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+  })
+  lastname: string;
+
+  @Column({
+    type: 'int',
+    nullable: true,
+  })
+  age: number;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+  })
+  profilepicture: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+  })
+  address: string;
+
+  @Column({
+      type : "varchar",
+      nullable : true  
+  })
+  phone  : string;
+
+  @CreateDateColumn() createdAt : Date;
+
+  @UpdateDateColumn() updatedAt : Date;
+}
