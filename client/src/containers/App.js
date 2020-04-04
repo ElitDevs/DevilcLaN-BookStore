751a2e91a8934dc51bb5afaef05eebbf23c8b2f7
larbibaraka <laridev@gmail.com>
2020-04-04 01:55:01 +0100

    creating redux architecture

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
- 
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-     <h1>hello Baraka Larbi</h1> 
+      <h1>hello Baraka Larbi</h1>
     </div>
   );
 }
