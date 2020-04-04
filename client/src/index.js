1135c426746dee82314fb5abe3e62ac74b1781b2
larbibaraka <laridev@gmail.com>
2020-04-04 02:11:05 +0100

    adding redux to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './index.scss';
+import "./index.scss";
 import App from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
 
+/** redux */
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+import { createLogger } from "redux-logger";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const middleware = [thunk, createLogger(rootReducer)];
+const store = createStore(rootReducer, {}, applyMiddleware(...middleware));
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
