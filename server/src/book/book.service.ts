65bcea288c736cc2b36ffeac6da1d9e3408639e5
Baraka Larbi <laridev@gmail.com>
2020-04-04 23:20:05 +0100

    many to many authors and books is working

--- server/src/book/book.service.ts
+++ server/src/book/book.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { BookEntity } from './book.entity';
 import { BookDto } from './book.dto';
 import { AuthorEntity } from './../author/author.entity';
+import e = require('express');
 
 @Injectable()
 export class BookService {
@@ -28,30 +29,11 @@ export class BookService {
   }
 
   async create(data): Promise<BookEntity> {
-    //get the data => books + data for authors and category
-    // console.log('data for book : ', data);
-    // bookname: "The Testaments: The Sequel to 'The Handmaid's Tale'",
-    // bookpages: 432,
-    // bookisbn: '978-0385543781',
-    // bookprice: 15,
-    // bookpicture: '',
-    // bookqauntity: 50,
-    // category: 1,
-    // authors: [ '26ecd3b0-7f19-401e-97fd-8fc1ad473c50' ]
-    // const book = await this.bookRepository.create({
-    //   bookname: data.bookname,
-    //   bookname: data.bookname,
-    //   bookpages: data.bookpages,
-    //   bookisbn: data.bookisbn,
-    //   bookprice: data.bookprice,
-    //   bookpicture: data.bookpicture,
-    //   bookqauntity: data.bookqauntity,
-    //   category: data.category,
-    //   authors: data.authors,
-    // });
+    const authors: AuthorEntity[] = await this.authorRepository.findByIds(
+      data.authors,
+    );
 
-    // const author = await this.authorRepository.findOne({ id: data.authors[0] });
-    // console.log('found author : ', author);
+    console.log('found author : ', authors);
     const book = new BookEntity();
     book.bookname = data.bookname;
     book.bookname = data.bookname;
@@ -61,10 +43,9 @@ export class BookService {
     book.bookpicture = data.bookpicture;
     book.bookqauntity = data.bookqauntity;
     book.categories = data.category;
-    book.authors = data.authors;
+    book.authors = authors;
     await this.bookRepository.save(book);
     return book;
-    // return await this.bookRepository.find();
   }
 
   async update(id: string, data: Partial<BookDto>): Promise<BookEntity> {
