08302bfb9f09769c0337545d421026871827b2fb
larbibaraka <laridev@gmail.com>
2020-04-04 02:04:18 +0100

    adding signin

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Register from '../components/register'
-
+import Register from "../components/register";
+import SignIn from "../components/signin";
 
 function App() {
   return (
     <div className="App">
-       <Register/>
+      <div className="row">
+        <div className="col-md-6">
+          <Register />
+        </div>
+        <div className="col-md-6">
+          <SignIn />
+        </div>
+      </div>
     </div>
   );
 }
